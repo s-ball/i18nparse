66aa3e9ca257ca1521998d7b0d65b1a56438c803
Serge Ballesta <sg-ball@laposte.net>
2018-11-14 23:25:06 +0100

    Add the pot template file to git.
    Also add some minor improvement to the french translation file.

--- i18nparse/i18nparse.py
+++ i18nparse/i18nparse.py
@@ -1,3 +1,5 @@
+""" This module allows to activate a translation for the module argparse
+"""
 import gettext
 import argparse
 import locale
@@ -7,14 +9,20 @@ _orig = { "_": argparse._, "ngettext": argparse.ngettext }
 
 locpath = os.path.dirname(__file__)
 def activate(lang=None):
+    """Activate a translation for lang.
+
+If lang is None, then the language of locale.getdefaultlocale() is used.
+If the translation file does not exist, the original messages will be used.
+"""
     if lang is None:
         lang = locale.getdefaultlocale()[0]
     tr = gettext.translation("argparse", os.path.join(locpath, "locale"),
-                             [lang])
+                             [lang], fallback=True)
     argparse._ = tr.gettext
     argparse.ngettext = tr.ngettext
 
 def deactivate():
+    """Revert to original messages"""
     argparse._ = _orig["_"]
     argparse.ngettext = _orig["ngettext"]
 
